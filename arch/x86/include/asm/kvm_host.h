0f8ede78372f6772e167be637aa73e51af5a903c
Xiaoguang Chen <xiaoguang.chen@intel.com>
2015-07-09 17:14:01 +0800

    kvmgt porting aperture and opregion intialization
    alloc/free,map/unmap the aperture and opregion in kvmgt
    
    Signed-off-by: Jike Song <jike.song@intel.com>
    Signed-off-by: XiaoGuang Chen <xiaoguang.chen@intel.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -35,7 +35,7 @@
 #define KVM_SOFT_MAX_VCPUS 160
 #define KVM_USER_MEM_SLOTS 509
 /* memory slots that are not exposed to userspace */
-#define KVM_PRIVATE_MEM_SLOTS 3
+#define KVM_PRIVATE_MEM_SLOTS 5
 #define KVM_MEM_SLOTS_NUM (KVM_USER_MEM_SLOTS + KVM_PRIVATE_MEM_SLOTS)
 
 #define KVM_PIO_PAGE_OFFSET 1
